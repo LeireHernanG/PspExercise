4258ba7c83991bb2dc36978e586a1ed4f38a0abd
LeireHernanG <166366742+LeireHernanG@users.noreply.github.com>
2025-01-17 12:39:08 +0100

    Upload of exercise
    Exercise 1 and 2

---
+++ pspEjercicio1/MusicianPracticeInstrument.java
@@ -0,0 +1,26 @@
+package pspEjercicio1;
+//Class for representing practincing of the musician
+
+public class MusicianPracticeInstrument extends Thread {
+    private String musicianName;
+    private int practiceTime;
+    private String songName;
+    
+    public MusicianPracticeInstrument(String musicianName, int practiceTime,String songName) {
+        this.musicianName = musicianName;
+        this.songName=songName;
+        this.practiceTime = practiceTime;//in seconds
+    }
+
+
+	public void run() {
+        System.out.println(musicianName + " started practicing the song "+songName);
+        try {
+            Thread.sleep(practiceTime * 1000L);//To convert seconds in milisecond
+        } catch (InterruptedException e) {
+            System.out.println(musicianName + " was interrupted while practicing");
+        }
+        System.out.println(musicianName + " finished practicing");
+    }
+}
+
